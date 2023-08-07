c31607b44398cd9ce12b75ffdb2bdba92088c396
Joshua Björkäng <joshua@mullvad.net>
2023-08-07 07:19:20 +0200

    scripts/generate_social_blocklists: Fix up typo on output file path

--- scripts/generate_social_blocklists.sh
+++ scripts/generate_social_blocklists.sh
@@ -10,5 +10,5 @@ mkdir -p /tmp/lists
 
 subfinder -dL generate_social_blocklists_urls --silent | \
     tee -a /tmp/lists/subfinder && \
-    cat /tmp/subfinder | \
+    cat /tmp/lists/subfinder | \
     awk '{ print "0.0.0.0 " $1 }' > /tmp/lists/output
